860f6191a2d7f7d004ff9a47053e43c66faed31b
SIVANANTHAM-PYTHON-LEARNER <102138544+SIVANANTHAM-PYTHON-LEARNER@users.noreply.github.com>
2022-03-22 12:28:41 +0530

    Add files via upload

---
+++ 123.py
@@ -0,0 +1,73 @@
+'''a=int(input('enter a number'))
+b=int(input('enter another number'))
+print(a+b)
+'''
+
+'''a=int(input('enter first no'))
+b=int(input('enter second no'))
+print(a-b)'''
+
+'''a=int(input('enter first no'))
+b=int(input("enter second no"))
+print(a*b)'''
+
+'''a=int(input('enter a no'))
+b=int(input('enter another no'))
+print('{}/{}'.format(a,b))'''
+
+'''a=int(input("enter a number"))
+print(a*a)'''
+
+'''a=int(input("enter a number"))
+print(a*a*a)'''
+
+'''a=int(input("enter the length"))
+b=int(input("enter the breadth"))
+print('area is ',a*b)'''
+
+'''a=int(input("enter length"))
+b=int(input("enter breadth"))
+c=int(input("enter height"))
+print('area of cuboid is ',(a*b*c))'''
+
+'''a=int(input("enter a number"))
+if(a>0):
+    print('{} is positive'.format(a))
+else:
+    print('{} is negative'.format(a))'''
+
+'''a=int(input('enter a number'))
+if((a%2==0)):
+    print('{} is even'.format(a))
+else:
+    print('{} is odd'.format(a))'''
+
+'''a=int(input('enter your age'))
+if(a>=18):
+    print(' U can vote')
+else:
+    print(' U cant vote')'''
+
+'''a=int(input('enter a number'))
+b=int(input("enter another number"))
+if(a>b):
+    print('{} is greater than {}'.format(a,b))
+else:
+    print(' {} is greater than {}'.format(b,a))'''
+
+'''a=int(input('enter first number'))
+b=int(input('enter second number'))
+c=int(input('enter third number'))
+if(a>b):
+    if(a>c):
+        print("{} is greatest".format(a))
+    else:
+        print("{} is greatest".format(c))
+else:
+    if(b>c):
+        print("{} is greatest".format(b))
+    else:
+        print("{} is greatest".format(c))'''
+        
+     
+
